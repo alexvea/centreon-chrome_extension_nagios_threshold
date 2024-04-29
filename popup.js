792e8c6ab4274f8443c5858f0866a0bec856a1bb
alexvea <35368807+alexvea@users.noreply.github.com>
2024-04-29 17:54:51 +0200

    Update popup.js
    decimal number case

--- popup.js
+++ popup.js
@@ -6,12 +6,33 @@ document.getElementById("random").addEventListener("click", random_value);
 function random_value() {
     let min = (document.getElementById("warning").value) ? document.getElementById("warning").value.replace("@", "").replace("~:", ""): document.getElementById("warning").placeholder;
     let max = (document.getElementById("critical").value) ? document.getElementById("critical").value.replace("@", "").replace("~:", ""): document.getElementById("critical").placeholder
-    document.getElementById("value").value = randomIntFromInterval(min, max);
+    document.getElementById("value").value = randomNumberFromInterval(min, max);
     test_thresholds();
 }
 
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (parseInt(max) + parseInt(min)));
+Number.prototype.countDecimals = function () {
+
+    if (Math.floor(this.valueOf()) === this.valueOf()) return 0;
+
+    var str = this.toString();
+    if (str.indexOf(".") !== -1 && str.indexOf("-") !== -1) {
+        return str.split("-")[1] || 0;
+    } else if (str.indexOf(".") !== -1) {
+        return str.split(".")[1].length || 0;
+    }
+    return str.split("-")[1] || 0;
+}
+
+
+function randomNumberFromInterval(min, max) { // min and max included
+    if (parseFloat(min) % 1 != 0 || parseFloat(max) % 1 != 0) {
+        var precision = ( parseFloat(min).countDecimals() != 0) ? parseFloat(min).countDecimals():parseFloat(max).countDecimals() ; 
+        var generatedNumber = Math.floor(Math.random() * (parseFloat(max) + parseFloat(min))) + Math.floor(Math.random() * (parseFloat(max) * precision - parseFloat(min) * precision) + parseFloat(min)) / (parseFloat(min));
+        generatedNumber = generatedNumber.toFixed(precision);
+    } else {
+        var generatedNumber = Math.floor(Math.random() * (parseFloat(max) + parseFloat(min)));
+    }
+    return generatedNumber;
 }
 
 
@@ -80,41 +101,41 @@ function check_first_rule(threshold_start,threshold_end) {
 
 function check_threshold(value,threshold_value,status) {
     switch (true) {
-        case /^(-|)\d*\d$/.test(threshold_value):
+        case /^(-|)\d*\.?\d*$/.test(threshold_value):
         //Range definition: 10 // Generate an alert if x... : < 0 or > 10, (outside the range of {0 .. 10})
-            threshold_reached = (parseInt(value) < 0 || parseInt(value) > parseInt(threshold_value)) ? "yes":"no";
+            threshold_reached = (parseFloat(value) < 0 || parseFloat(value) > parseFloat(threshold_value)) ? "yes":"no";
             explanation_threshold_content += "Range definition: "+threshold_value+" for "+status+ " threshold. Generate an alert if current value "+value+" is < 0 or > "+threshold_value+", (outside the range of {0 .. "+threshold_value+"}. Triggered : "+threshold_reached+"<br>";
         break;
-        case /^(-|)\d*:$/.test(threshold_value):
+        case /^(-|)\d*\.?\d*:$/.test(threshold_value):
         //Range definition: 10: // Generate an alert if x... : < 10, (outside {10 .. ∞})
             threshold_value = threshold_value.substring(0, threshold_value.length - 1);
-            threshold_reached = (parseInt(value) < parseInt(threshold_value)) ? "yes":"no";
+            threshold_reached = (parseFloat(value) < parseFloat(threshold_value)) ? "yes":"no";
             explanation_threshold_content += "Range definition: "+threshold_value+" for "+status+ " threshold. Generate an alert if current value "+value+" is < "+threshold_value+", (outside {"+threshold_value+" .. &infin;}. Triggered : "+threshold_reached+"<br>";
         break;
-        case /^~:(-|)\d*\d$/.test(threshold_value):
+        case /^~:(-|)\d*\.?\d*$/.test(threshold_value):
         //Range definition: ~:10 // Generate an alert if x... : > 10, (outside the range of {-∞ .. 10})
             threshold_value = threshold_value.substring(2);
-            threshold_reached = (parseInt(value) > parseInt(threshold_value)) ? "yes":"no";
+            threshold_reached = (parseFloat(value) > parseFloat(threshold_value)) ? "yes":"no";
             explanation_threshold_content += "Range definition: "+threshold_value+" for "+status+ " threshold. Generate an alert if current value "+value+" is > "+threshold_value+", (outside the range of {-&infin; .. "+threshold_value+"}. Triggered : "+threshold_reached+"<br>";
         break;
-        case /^(-|)\d*:(-|)\d*$/.test(threshold_value):
+        case /^(-|)\d*\.?\d*:(-|)\d*\.?\d*$/.test(threshold_value):
         //Range definition: 10:20 // Generate an alert if x... : < 10 or > 20, (outside the range of {10 .. 20})
             array_threshold_value = threshold_value.split(":");
             threshold_value_left = array_threshold_value[0];
             threshold_value_right = array_threshold_value[1];
             //TODO skip if rule first rule not respected
-            console.log(check_first_rule(parseInt(threshold_value_left),parseInt(threshold_value_right)));
-            threshold_reached = (parseInt(value) <  parseInt(threshold_value_left) || parseInt(value) > parseInt(threshold_value_right)) ? "yes":"no";
+            console.log(check_first_rule(parseFloat(threshold_value_left),parseFloat(threshold_value_right)));
+            threshold_reached = (parseFloat(value) <  parseFloat(threshold_value_left) || parseFloat(value) > parseFloat(threshold_value_right)) ? "yes":"no";
             explanation_threshold_content += "Range definition: "+threshold_value+" for "+status+ " threshold. Generate an alert if current value "+value+" is < "+threshold_value_left+" or > "+threshold_value_right+", (outside the range of {"+threshold_value_left+" .. "+threshold_value_right+"}. Triggered : "+threshold_reached+"<br>";
         break;
-        case /^@(-|)\d*:(-|)\d*$/.test(threshold_value):
+        case /^@(-|)\d*\.?\d*:(-|)\d*\.?\d*$/.test(threshold_value):
         //Range definition: @10:20 // Generate an alert if x... : <= 10 and >= 20, (inside the range of {10 .. 20})
             array_threshold_value = threshold_value.split(":");
             threshold_value_left = array_threshold_value[0].substring(1);
             threshold_value_right = array_threshold_value[1];
             //TODO skip if rule first rule not respected
-            console.log(check_first_rule(parseInt(threshold_value_left),parseInt(threshold_value_right)));
-            threshold_reached = (parseInt(value) >=  parseInt(threshold_value_left) && parseInt(value) <= parseInt(threshold_value_right)) ? "yes":"no";
+            console.log(check_first_rule(parseFloat(threshold_value_left),parseFloat(threshold_value_right)));
+            threshold_reached = (parseFloat(value) >=  parseFloat(threshold_value_left) && parseFloat(value) <= parseFloat(threshold_value_right)) ? "yes":"no";
             explanation_threshold_content += "Range definition: "+threshold_value+" for "+status+ " threshold. Generate an alert if current value "+value+" is &#8805; "+threshold_value_left+" and &#8804; "+threshold_value_right+", (inside the range of {"+threshold_value_left+" .. "+threshold_value_right+"}. Triggered : "+threshold_reached+"<br>";
         break;
         default:
